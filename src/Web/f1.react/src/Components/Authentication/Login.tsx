7e7bda3a71b2c2549c9afe1f644e33ae2d80e068
Dav Evans <dav@dav-evans.com>
2021-12-31 18:20:11 +1100

    Update css

--- src/Web/f1.react/src/Components/Authentication/Login.tsx
+++ src/Web/f1.react/src/Components/Authentication/Login.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import { AuthenticationContext } from './AuthenticationContext';
+import { Form, Input, Button } from 'antd';
 
 const Login = () => {
     let navigate = useNavigate();
@@ -8,9 +9,9 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const onClick = async (event: any) => {
-        event.preventDefault();
-        authenticationContext?.login({ username: email, password})
+
+    const onFinish = (values: any) => {
+        authenticationContext?.login({ username: values.email, password: values.password})
             .then(_ => {
                 console.log('logged in.');
                 navigate("/");
@@ -21,26 +22,35 @@ const Login = () => {
     }
 
     return (
-        <div>
-            <h1>Login</h1>
-            <div>
-                <form>
-                    <label>
-                        Email:
-                        <input type="email" value={email} onChange={(e) => setEmail(e.currentTarget.value)} required />
-                    </label>
-                    <label>
-                        Password:
-                        <input type="password" value={password} onChange={(e) => setPassword(e.currentTarget.value)} required/>
-                    </label>
+        <div id="loginContainer">
+            <div id="login">
+                <h1>Login</h1>
+                <Form 
+                onFinish={onFinish}
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                
+                initialValues={{ remember: true }} autoComplete="off">
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email' }]}>
+                        <Input value={email} onChange={(e) => setEmail(e.currentTarget.value)}/>
+                    </Form.Item>
 
-                    <button onClick={onClick}>Sign In</button>
-                </form>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password' }]}>
+                        <Input.Password value={password} onChange={(e) => setPassword(e.currentTarget.value)}/>
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit">Login</Button>
+                    </Form.Item>
+                </Form>  
             </div>
-
-            <Link to="/register">Register</Link>
         </div>
-
     )
 }
 
